4c603c3f5ef605e2dcdfb739897d999f4c3ab261
Eoghan O'Sullivan <moo.101x2@gmail.com>
2022-06-02 12:59:43 +0000

    Add shipments overview page

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', page_title="Dukes Inventory")
+
+@app.route('/shipments')
+def shipments():
+	return render_template('shipments.html', page_title="Dukes Shipments")
 
 app.run(host='0.0.0.0', port=81)
