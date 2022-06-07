e3ec0e3e79a9d4683551cffb72cdd0838cc85d8a
Eoghan O'Sullivan <moo.101x2@gmail.com>
2022-06-07 21:01:27 +0000

    Update create product route to allow data entry, update index page to show products dynamically

--- main.py
+++ main.py
@@ -1,12 +1,43 @@
 from flask import Flask, render_template, redirect, flash, request
 import os
+import psycopg2
+import random
+import string
+
+
 
 app = Flask(__name__)
 app.secret_key = os.getenv('SECRET_KEY')
 
+def keyGenerator():
+	upper_string = string.ascii_uppercase
+	lower_string = string.ascii_lowercase
+	numerical = string.digits
+	
+	alphabet = upper_string + lower_string + numerical
+	random_character = random.sample(alphabet, 12)
+	key = "".join(random_character)
+	return key
+
+def connect_db():
+	"""Connects to the database"""
+	database_uri = os.environ['DATABASEURI']
+	db_connection = psycopg2.connect(database_uri, sslmode="require")
+	return db_connection
+
+
 @app.route('/')
 def index():
-    return render_template('index.html', page_title="Dukes Inventory")
+	connection = connect_db()
+	cursor = connection.cursor()
+	
+	search_query = f"SELECT * FROM product"
+	
+	cursor.execute(search_query)
+	connection.commit()
+	
+	product_result = cursor.fetchall()
+	return render_template('index.html',page_title="Dukes Inventory", products = product_result)
 
 @app.route('/shipments')
 def shipments():
@@ -23,6 +54,22 @@ def updateProduct():
 
 @app.route('/create-product', methods=("GET", "POST"))
 def createProdcut():
+	db = connect_db()
+	cursor = db.cursor()
+	
+	product_key = keyGenerator(),
+	product_name = request.form.get('productTitle'),
+	product_qquantity = request.form.get('productQuantity'),
+	product_sku = request.form.get('productSKU'),
+	product_image = request.form.get('productMedia'),
+	product_description = request.form.get('productDescription')
+
+	insert_query = """INSERT INTO product (product_key, product_name,product_qquantity,product_sku, product_image, product_description) VALUES (%s,%s,%s,%s,%s,%s)"""
+
+	record_to_insert = (product_key, product_name,product_qquantity,product_sku, product_image, product_description)
+	cursor.execute(insert_query, record_to_insert)
+	db.commit()
+
 	flash('Product created sucessfully','success')
 	return redirect('/')
 
